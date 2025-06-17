66174ee0540d3b494fcd3f946bb2789034552dbd
Jorge Arthur Biavath <jorge.biavath@gmail.com>
2025-06-17 18:00:58 -0300

    rollback layout

--- nolan-diagram-app/nolan-diagram-app/app/layout.js
+++ nolan-diagram-app/nolan-diagram-app/app/layout.js
@@ -1,30 +1,14 @@
-//import "./globals.css";
+import "./globals.css";
 
-//export const metadata = {
-  //title: "Teste de Espectro Político",
-  //description: "Teste de espectro político baseado no Diagrama de Nolan.",
-//};
+export const metadata = {
+  title: "Teste de Espectro Político",
+  description: "Teste de espectro político baseado no Diagrama de Nolan.",
+};
 
-//export default function RootLayout({ children }) {
-  //return (
-    //<html lang="pt-BR">
-      //<body>{children}</body>
-    //</html>
-  //)
-//}
-
-import { Analytics } from '@/package.json';
- 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <head>
-        <title>Teste de Espectro Político</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
