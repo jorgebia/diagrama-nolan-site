9ba9ad25261c133d017579f05efd7f06e06ff076
Jorge Arthur Biavath <jorge.biavath@gmail.com>
2025-06-17 23:28:39 -0300

    add image

--- nolan-diagram-app/app/layout.js
+++ nolan-diagram-app/app/layout.js
@@ -1,5 +1,23 @@
 import "./globals.css";
 
+export const metadata = {
+  description: "Descubra sua posição política com este quiz.",
+  openGraph: {
+    description: "Descubra sua posição política com este quiz.",
+    url: "https://testepoliticobr.vercel.app/",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 800,
+        height: 800,
+        alt: "Diagrama de Nolan"
+      }
+    ],
+    locale: "pt_BR",
+    type: "website"
+  }
+};
+
 //export const metadata = {
   //title: "Teste de Espectro Político",
   //description: "Teste de espectro político baseado no Diagrama de Nolan.",
