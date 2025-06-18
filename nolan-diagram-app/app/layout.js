ff8e8917320d5574bacccffbe0104141f6587958
Jorge Arthur Biavath <jorge.biavath@gmail.com>
2025-06-17 22:37:37 -0300

    update layout

--- nolan-diagram-app/app/layout.js
+++ nolan-diagram-app/app/layout.js
@@ -1,14 +1,30 @@
 import "./globals.css";
 
-export const metadata = {
-  title: "Teste de Espectro Político",
-  description: "Teste de espectro político baseado no Diagrama de Nolan.",
-};
+//export const metadata = {
+  //title: "Teste de Espectro Político",
+  //description: "Teste de espectro político baseado no Diagrama de Nolan.",
+//};
 
+//export default function RootLayout({ children }) {
+  //return (
+    //<html lang="pt-BR">
+      //<body>{children}</body>
+    //</html>
+  //)
+//}
+
+import { Analytics } from '@vercel/analytics/next';
+ 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <head>
+        <title>Teste de Espectro Político</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
