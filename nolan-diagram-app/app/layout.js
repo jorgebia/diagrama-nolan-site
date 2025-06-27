90b5ad2d0987b4f3d62c928e0e29c93bf9a76765
Jorge Arthur Biavath <jorge.biavath@gmail.com>
2025-06-26 22:21:18 -0300

    update layout descrições

--- nolan-diagram-app/app/layout.js
+++ nolan-diagram-app/app/layout.js
@@ -4,10 +4,10 @@ export const metadata = {
   description: "Descubra sua posição política com este quiz.",
   openGraph: {
     description: "Descubra sua posição política com este quiz.",
-    url: "https://testepoliticobr.vercel.app/",
+    url: "https://quizpoliticobr.vercel.app/",
     images: [
       {
-        url: "https://testepoliticobr.vercel.app/og-image.jpg",
+        url: "https://quizpoliticobr.vercel.app/og-image.jpg",
         width: 800,
         height: 800,
         alt: "Diagrama de Nolan"
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        <title>Teste de Espectro Político</title>
+        <title>Quiz Político Brasil</title>
       </head>
       <body>
         {children}
