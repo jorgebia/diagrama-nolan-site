5f874edc9b5378df8fe183551af6ed64ace94f6c
Jorge Arthur Biavath <jorge.biavath@gmail.com>
2025-07-30 10:51:43 -0300

    add icon no layout

--- nolan-diagram-app/app/layout.js
+++ nolan-diagram-app/app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
     <html lang="pt-BR">
       <head>
         <title>Quiz Político Brasil</title>
+        <link rel="icon" href="/favicon.png" type="image/png" />
       </head>
       <body>
         {children}
