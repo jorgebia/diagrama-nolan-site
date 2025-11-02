2cfc1c42ae3b8e1c9b35edf8cc9731bba262d395
Jorge Arthur Biavath <jorge.biavath@gmail.com>
2025-11-01 21:11:56 -0300

    update page

--- nolan-diagram-app/app/page.js
+++ nolan-diagram-app/app/page.js
@@ -174,8 +174,8 @@ function getIdeologyLabel(economic, social) {
       {submitted && (
         <section ref={resultRef}>
           <h2 className="text-2xl font-bold mb-4 text-center">Seu Resultado</h2>
-          <div className="mt-10 sm:mt-12 w-full h-96">
-            <ResultDiagram economic={economic} social={social} className="w-full h-full"/>
+          <div className="mt-10 sm:mt-12 max-w-[500px] mx-auto">
+            <ResultDiagram economic={economic} social={social}/>
           </div>
 
           {(() => {
