84bd0b71320a9d4b499217014ae82032a7be09a8
Jorge Arthur Biavath <jorge.biavath@gmail.com>
2026-02-16 13:11:56 -0300

    ajuste fino palavras eixos

--- nolan-diagram-app/app/components/ResultDiagram.js
+++ nolan-diagram-app/app/components/ResultDiagram.js
@@ -46,26 +46,26 @@ export default function ResultDiagram({ economic, social }) {
     
 
           {/* Descrições dos Eixos*/}
-        <div className="absolute -top-8 left-0 w-full text-center">
+        <div className="absolute -top-7 left-0 w-full text-center">
           <span className="text-[10px] sm:text-[11px] font-black uppercase text-slate-600 bg-white/90 px-2">
             Libertário
           </span>
         </div>
         
-        <div className="absolute -bottom-8 left-0 w-full text-center">
+        <div className="absolute -bottom-7 left-0 w-full text-center">
           <span className="text-[10px] sm:text-[11px] font-black uppercase text-slate-600 bg-white/90 px-2">
             Autoritário
           </span>
         </div>
 
         {/* Esquerda e Direita (Rotacionadas) */}
-        <div className="absolute top-0 -left-8 h-full flex items-center">
+        <div className="absolute inset-y-0 -left-10 flex items-center">
           <span className="transform -rotate-90 origin-center text-[10px] sm:text-[11px] font-black uppercase text-slate-600 whitespace-nowrap">
             Esquerda
           </span>
         </div>
 
-        <div className="absolute top-0 -right-8 h-full flex items-center">
+        <div className="absolute inset-y-0 -right-10 flex items-center">
           <span className="transform rotate-90 origin-center text-[10px] sm:text-[11px] font-black uppercase text-slate-600 whitespace-nowrap">
             Direita
           </span>
